aa1089f6e36facf881cfe10108de6bf030d33130
Neel Patel <patel.neel5@northeastern.edu>
2024-03-31 16:30:39 -0400

    Add colors to safelist for dynamic rendering

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  safelist: [
+    "bg-squareblack",
+    "bg-squarewhite",
+    "text-squareblack",
+    "text-squarewhite",
+  ],
   theme: {
     extend: {
       colors: {
-        darkgreen: "#769656",
-        offwhite: "#eeeed2",
+        squareblack: "#769656",
+        squarewhite: "#eeeed2",
         lightgreen: "#baca44",
         primary: "#ffffff",
         secondary: "#000000",
+      },
+      fontFamily: {
+        primary: ["Montserrat", "sans-serif"],
       },
     },
   },
