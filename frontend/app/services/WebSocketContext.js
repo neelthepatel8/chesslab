791c24771b96d64deafa09470f17ff560507c64f
Neel Patel <patel.neel5@northeastern.edu>
2024-04-06 15:48:49 -0400

    Add missing import

--- frontend/app/services/WebSocketContext.js
+++ frontend/app/services/WebSocketContext.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { createContext, useState, useEffect, useContext } from "react";
-
+import log from "../utils/log";
 const WebSocketContext = createContext(null);
 
 export const useWebSocket = () => useContext(WebSocketContext);
