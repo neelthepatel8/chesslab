e822582cb13788775ddc03a2cd0af31cdf424cb2
Neel Patel <patel.neel5@northeastern.edu>
2025-03-13 14:54:43 -0400

    Update page.js

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -5,7 +5,8 @@ export default function Home() {
   return (
     <WebSocketProvider>
       <main className="flex h-screen w-screen items-center justify-center">
-        <Board />
+        // <Board />
+          <h1>Currently Down due to an issue, work in progress. Expect updates soon.</h1>
       </main>
     </WebSocketProvider>
   );
